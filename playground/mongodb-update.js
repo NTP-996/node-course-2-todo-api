9ef8f883745e10164a973e3b266ef13362b70721
NTTMai <thaiphuongn1@gmail.com>
2018-07-06 15:17:18 +0700

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,38 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MonggoDB server');
+
+    // db.collection('Todos').findOneAndUpdate({
+    //     _id: new ObjectID('5b3c7802c081b6d82dacc9b1')
+    // }, {
+    //     $set: {
+    //         completed: true
+    //     }
+    // }, {
+    //     returnOriginal: false
+    // }).then((result) => {
+    //     console.log(result);
+    // });
+
+    db.collection('Users').findOneAndUpdate({
+        _id: new ObjectID('5b3c3d4c26d90a49480776da')
+    }, {
+        $set: {
+            name: 'NTPhuong'
+        }, 
+        $inc: {
+            age: 1
+        }
+    }, {
+        returnOriginal: false
+    }).then((result) => {
+        console.log(result);
+    });
+
+    // db.close;
+});
