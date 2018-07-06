b0fc81beee0f0fb00f1b40937e206d1250568906
NTTMai <thaiphuongn1@gmail.com>
2018-07-06 09:36:39 +0700

    add delete srcipt

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,29 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MonggoDB server');
+
+    // db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((result) => {
+    //     console.log(result);
+    // });
+
+    // db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((result) => {
+    //     console.log(result);
+    // })
+
+    // db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+    //     console.log(result);
+    // });
+
+    db.collection('Users').findOneAndDelete({
+        _id: new ObjectID('5b3ed422c081b6d82dacde61')
+    }).then((result) => {
+        console.log(result);
+    });
+
+    // db.close;
+});
